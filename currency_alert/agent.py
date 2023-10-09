a024e1deba6d3033504b1f428b546980f35d076c
itsmeherepranetha <pranethacoding@gmail.com>
2023-10-09 19:45:20 +0530

    currency_ui

--- currency_alert/agent.py
+++ currency_alert/agent.py
@@ -3,9 +3,9 @@ import requests
 bureau=Bureau()
 from Main.utils import *
 
-a = Agent(name="a", seed="a recovery phrase")
+Pranetha = Agent(name="Pranetha", seed="Pranetha recovery phrase")
 
-@a.on_interval(period=2.0)
+@Pranetha.on_interval(period=2.0)
 async def say_hello(ctx: Context):
     url = "https://currency-exchange.p.rapidapi.com/exchange"
 
@@ -18,20 +18,20 @@ async def say_hello(ctx: Context):
 
     response = requests.get(url, headers=headers, params=querystring)
     ctx.storage.set("present", response.json())
-    ctx.storage.set("max_value", 100.00)
-    ctx.storage.set("min_value", 1.00)
+    ctx.storage.set("max_value", 81.00)
+    ctx.storage.set("min_value", 80.00)
     if ctx.storage.get("present")>ctx.storage.get("max_value") or ctx.storage.get("present")<ctx.storage.get("min_value"):
         ctx.logger.info(f'\033[91mNot in range\033[0m')
     else:
         ctx.logger.info(f'\033[94mIn range\033[0m')
 
-def run_a():
-    a.run()
-bureau.add(a)
+def run_Pranetha():
+    Pranetha.run()
+bureau.add(Pranetha)
 
-b = Agent(name="b", seed="b recovery phrase")
+abcd = Agent(name="abcd", seed="abcd recovery phrase")
 
-@b.on_interval(period=2.0)
+@abcd.on_interval(period=2.0)
 async def say_hello(ctx: Context):
     url = "https://currency-exchange.p.rapidapi.com/exchange"
 
@@ -44,13 +44,13 @@ async def say_hello(ctx: Context):
 
     response = requests.get(url, headers=headers, params=querystring)
     ctx.storage.set("present", response.json())
-    ctx.storage.set("max_value", 200.00)
-    ctx.storage.set("min_value", 100.00)
+    ctx.storage.set("max_value", 84.00)
+    ctx.storage.set("min_value", 82.00)
     if ctx.storage.get("present")>ctx.storage.get("max_value") or ctx.storage.get("present")<ctx.storage.get("min_value"):
         ctx.logger.info(f'\033[91mNot in range\033[0m')
     else:
         ctx.logger.info(f'\033[94mIn range\033[0m')
 
-def run_b():
-    b.run()
-bureau.add(b)
+def run_abcd():
+    abcd.run()
+bureau.add(abcd)
